6ce02f6159e22636a2bf23e8729e4b9e1a1c9ee3
triple486 <triple486@gmail.com>
2022-06-14 10:40:46 +0900

    add a shopping cart component

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,27 @@
+import classes from "./Cart.module.css";
+
+const Cart = (props) => {
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <div>
+      {cartitems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <spna>35.62</spna>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
